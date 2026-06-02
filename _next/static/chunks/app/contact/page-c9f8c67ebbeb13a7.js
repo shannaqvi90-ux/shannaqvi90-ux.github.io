2196a3da565911c300d56b96bbb5ea4a99b62bfc
Shannaqvi <35780941+Shannaqvi@users.noreply.github.com>
2026-06-02 20:30:59 +0400

    Update site

---
+++ _next/static/chunks/app/contact/page-c9f8c67ebbeb13a7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327,301,348,385,906],{1080:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=1080)}),_N_E=n.O()}]);
