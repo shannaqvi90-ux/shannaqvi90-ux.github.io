2239c599c8ef0722dbba61a7f29d39813a54e9a2
Shannaqvi <35780941+Shannaqvi@users.noreply.github.com>
2026-06-02 11:44:26 +0400

    Publish UAE mortgage tools site

---
+++ _next/static/chunks/app/disclaimer/page-04f03151184fb449.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[348,301,327,906,385],{1080:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=1080)}),_N_E=n.O()}]);
